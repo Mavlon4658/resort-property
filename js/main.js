a1e80e7add89bf3a5cad67354d400a7277a0c30e
Mavlon4658 <mavlonbe4658>
2024-08-09 09:30:40 +0500

    home is ready

---
+++ js/main.js
@@ -0,0 +1,113 @@
+let dropdowns = document.querySelectorAll('.dropdown');
+
+if (dropdowns.length) {
+    dropdowns.forEach(dropdown => {
+        let btn = dropdown.querySelector('.dropdown-head'),
+        list = dropdown.querySelector('.dropdown-body'),
+        listItem = dropdown.querySelectorAll('.dropdown-body li');
+        
+        if (btn) {
+            btn.onclick = () => {
+                if (list.classList.contains('flex')) {
+                    list.classList.remove('flex');
+                    list.classList.add('hidden');
+                } else {
+                    list.classList.add('flex');
+                    list.classList.remove('hidden');
+                }
+            }
+
+            listItem.forEach(el => {
+                el.onclick = () => {
+                    btn.querySelector('span').textContent = el.textContent;
+                    list.classList.remove('flex');
+                    list.classList.add('hidden');
+                }
+            })
+        }
+    })
+}
+
+var init = false;
+var swiper;
+function swiperCard() {
+    if (window.innerWidth <= 768) {
+        if (!init) {
+        init = true;
+        swiper = new Swiper(".slider-cards-js", {
+            slidesPerView: "auto",
+            spaceBetween: 11,
+        });
+        }
+    } else if (init) {
+        swiper.destroy();
+        init = false;
+    }
+}
+
+if (document.querySelector('.best_deals__paretn_swp')) {
+    swiperCard();
+}
+
+let bestDealsChildChildSwp = document.querySelectorAll('.best_deals__child_swp');
+
+if (bestDealsChildChildSwp.length) {
+    bestDealsChildChildSwp.forEach(el => {
+        let swp = new Swiper(el, {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            effect: 'fade',
+            loop: true,
+            pagination: {
+                el: el.querySelector('.swp_pagination'),
+                clickable: true,
+            },
+            navigation: {
+                nextEl: el.querySelectorAll('button')[0],
+                prevEl: el.querySelectorAll('button')[1],
+            }
+        })
+    })
+}
+
+window.addEventListener("resize", () => {
+    if (document.querySelector('.best_deals__paretn_swp')) {
+        swiperCard();
+    }
+});
+
+let tabs = document.querySelectorAll('.tab');
+
+if (tabs.length) {
+    tabs.forEach(el => {
+        let tabBtns = el.querySelectorAll('.tab-head button');
+
+        if (tabBtns.length) {
+            tabBtns.forEach(btn => {
+                btn.onclick = () => {
+                    tabBtns.forEach(item => {
+                        if (item == btn) {
+                            item.classList.add('active');
+                        } else {
+                            item.classList.remove('active');
+                        }
+                    })
+                }
+            })
+        }
+    })
+}
+
+document.addEventListener('click', event => {
+    if (dropdowns.length) {
+        dropdowns.forEach(el => {
+            const isClickInside = el.contains(event.target);
+            let list = el.querySelector('.dropdown-body');
+        
+            if (!isClickInside) {
+                list.classList.remove('flex');
+                list.classList.add('hidden');
+            }
+        })
+    }
+})
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './css/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
