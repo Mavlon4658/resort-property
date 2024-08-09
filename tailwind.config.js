2f0fb31261a8e542fe133eed0c67562efdb1dcac
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-08-10 00:58:17 +0500

    comment the end

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['./index.html', './css/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Montserrat: ['"Montserrat"'],
+        Unbounded: ['"Unbounded"'],
+        ArgentumSans: ['"ArgentumSans"'],
+        Muller: ['"Muller"'],
+      },
+      colors: {
+        "gray": "#3D3D3D",
+        "line-border": "#DBDBDB",
+        "card-circle": "#D9D9D9",
+        "text-grey": "#00000066",
+        "text-grey-light": "#36363699",
+        "coral": "#EF924F",
+        "date": "#3D3D3D99",
+        "green-dark": "#3C606F",
+        "input-dark": "#FFFFFFB2",
+
+      },
+    },
   },
   plugins: [],
 }
